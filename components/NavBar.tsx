464701093c39042f1c184ca3f74497d91bd76edb
terryhycheng <terryhycheng@gmail.com>
2023-04-10 00:20:26 +0100

    Modified layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 const NavBar = () => {
   return (
-    <div className="border-b border-gray-300 p-4">
-      <div className="flex gap-4">
+    <header className="border-b border-gray-300 p-4">
+      <nav className="flex gap-4">
         <Link href="/">Home</Link>
         <Link href="/items">Items</Link>
-      </div>
-    </div>
+      </nav>
+    </header>
   );
 };
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,9 @@ import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <main>
+        <Component {...pageProps} />
+      </main>
     </Layout>
   );
 }
