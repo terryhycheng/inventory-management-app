521d6b1ff796364e4046923feca11ae9f249f32f
terryhycheng <terryhycheng@gmail.com>
2023-04-24 23:07:09 +0100

    Added logics to display `AddItemForm` modal

--- pages/items.tsx
+++ pages/items.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react';
 import AddItemForm from '@/components/AddItemForm';
 import { ICategory } from '@/server/models/category.model';
-import { IItem } from '@/server/models/item.model';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 import { GetStaticProps } from 'next';
 import ItemBox, { ItemData } from '@/components/ItemBox';
+import { useContext } from 'react';
+import { ModalContext } from '@/contexts/ModalContext';
 
 interface Props {
   items: ItemData[];
@@ -14,18 +14,22 @@ interface Props {
 
 const Items = ({ items, categories }: Props) => {
   const router = useRouter();
+  const modalStatus = useContext(ModalContext);
+
   return (
     <>
-      <hr />
-      <AddItemForm categories={categories} router={router} />
-      <hr />
+      {modalStatus?.isItemModal && (
+        <div className="fixed left-0 top-0 flex h-screen w-screen items-center justify-center bg-black bg-opacity-70">
+          <AddItemForm categories={categories} router={router} />
+        </div>
+      )}
       {items.length === 0 && <p>There is no item in the list.</p>}
       {items.length !== 0 && (
         <>
           <p className="my-4 font-light text-[#AAAAAA]">
             There are total {categories.length} kinds of items in the inventory.
           </p>
-          <div className="grid gap-4 lg:grid-cols-3">
+          <div className="grid gap-6 lg:grid-cols-3">
             {items.map((item) => (
               <ItemBox key={item._id} item={item} />
             ))}
